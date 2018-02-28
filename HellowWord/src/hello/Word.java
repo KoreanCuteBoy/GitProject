d17bea3ad1ea08b7d3aa8abc6d27f9ea05f18394
user <user@DESKTOP-6MNAU0T>
2018-02-28 14:09:49 +0900

    Word.java 1 file

---
+++ HellowWord/src/hello/Word.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class Word {
+
+	public static void main(String[] args) {
+		
+		helloword();
+	}
+	
+	public static void helloword()
+	{
+		System.out.println("HelloWord");
+	}
+
+}
