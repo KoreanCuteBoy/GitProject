bbe83e073a2e83df9c184784d5e71a546f5cc230
godofshy <final_1945@naver.com>
2018-02-28 17:15:57 +0900

    aaa추가

---
+++ HellowWord/src/hello/HelloWord.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		speech();
+
+	}
+	
+	public static void speech()
+	{
+		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+	}
+
+}
