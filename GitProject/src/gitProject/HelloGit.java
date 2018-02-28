f98fb601446520802f51ec1a59330a534a8241a5
godofshy <final_1945@naver.com>
2018-02-28 17:25:39 +0900

    첫 작업 프로젝트

---
+++ GitProject/src/gitProject/HelloGit.java
@@ -0,0 +1,14 @@
+package gitProject;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		speech();
+
+	}
+	
+	public static void speech()
+	{
+		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+	}
+}
